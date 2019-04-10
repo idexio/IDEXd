4f69c90b5af75aa0a02368435fdce34a1d4b7fe3
newzcard <yennieb@gmail.com>
2019-04-10 13:00:44 -0700

    fixed bad pm2config

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,6 @@ module.exports = {
     script: 'lib/index.js',
     instances: 1,
     autorestart: true,
-    watch: false,
     kill_timeout: 5000,
     exp_backoff_restart_delay: 1000,
     watch: "downtime.log",
